de1c58082ad8068f886bb0a934a02665c517d6a6
Zacharia Ndinguri <zacthuku@gmail.com>
2025-03-04 13:15:34 +0300

    strings lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+// Write your code in this file
+const currentUser="Grace Hopper";
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
+const excitedWelcomeMessage= welcomeMessage.toUpperCase();
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting= 'Welcome, ' + firstInitial + "!";
